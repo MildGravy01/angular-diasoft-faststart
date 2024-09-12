a83d40b20556f4e85e8e8cc17bc9dea33f2093ce
MildGravy0 <152@152>
2024-09-12 13:28:25 +0300

    fix: changed interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    LogoComponent
   ],
   imports: [
     BrowserModule,
