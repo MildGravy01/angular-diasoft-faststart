ac0bb6e8c465b002eb1652709f4417d48b89e32d
MildGravy0 <152@152>
2024-09-14 14:31:13 +0300

    feat: addded handler to load more button

--- src/app/pages/courses-page/courses-list-page/components/course-list/course-list.component.ts
+++ src/app/pages/courses-page/courses-list-page/components/course-list/course-list.component.ts
@@ -19,6 +19,11 @@ export class CourseListComponent {
     console.log('Course', courseItem);
    }
 
+
+   onLoadMore(){
+    console.log("Loading more...");
+   }
+
    deleteCourse(id:string){
     console.log('Delete', id);
    }
